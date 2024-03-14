704bf18700a5bbe0cfd196d68492d001770399af
Peter Lakics <71771187+dhemeira@users.noreply.github.com>
2024-03-14 15:41:16 +0100

    Add tailwind and setup colors (#4)
    resolve #5

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,49 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { RouterLink, RouterView } from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
+const hue = ref(200);
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <nav>
+  <div class="wrapper">
+    <header>
+      <nav class="flex gap-2 items-center justify-center">
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/week">Week</RouterLink>
       </nav>
-    </div>
-  </header>
+      <div class="flex gap-2 items-center justify-center">
+        <input
+          v-model="hue"
+          type="range"
+          min="0"
+          max="360" />
+        {{ hue }}
+      </div>
+    </header>
 
-  <RouterView />
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.wrapper {
+  --hue: v-bind(hue);
+  --bkg: var(--hue) 50% 94%;
+  --text: var(--hue) 80% 20%;
+  --bkg-dark: var(--hue) 50% 13%;
+  --text-dark: var(--hue) 80% 90%;
+  background-color: hsl(var(--bkg));
+  color: hsl(var(--text));
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+@media (prefers-color-scheme: dark) {
+  .wrapper {
+    background-color: hsl(var(--bkg-dark));
+    color: hsl(var(--text-dark));
   }
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.router-link-exact-active {
+  @apply text-xl font-bold;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,32 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue';
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="flex">
+    <div class="light h-screen w-1/2 p-4 flex items-center justify-center flex-col gap-8">
+      <h2 class="text-2xl font-bold">Light mode</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti adipisci exercitationem
+        cumque possimus at ex dolores. Distinctio, ullam placeat dicta non eius quo veniam corporis
+        cupiditate possimus sequi neque? In?
+      </p>
+    </div>
+    <div class="dark h-screen w-1/2 p-4 flex items-center justify-center flex-col gap-8">
+      <h2 class="text-2xl font-bold">Dark mode</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti adipisci exercitationem
+        cumque possimus at ex dolores. Distinctio, ullam placeat dicta non eius quo veniam corporis
+        cupiditate possimus sequi neque? In?
+      </p>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.dark {
+  background-color: hsl(var(--bkg-dark));
+  color: hsl(var(--text-dark));
+}
+
+.light {
+  background-color: hsl(var(--bkg));
+  color: hsl(var(--text));
+}
+</style>
