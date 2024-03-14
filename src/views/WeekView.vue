8b8fe8527d244b15c30c88bf7af6b326eef1d565
Dhemeira <peterlakics2@gmail.com>
2024-03-14 15:38:55 +0100

    Add tailwind and setup colors

---
+++ src/views/WeekView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex items-center justify-center min-h-full">
+    <div class="w-52 h-52 rounded-xl bg-white/5 flex items-center justify-center">
+      Current week: 5th
+    </div>
+  </div>
+</template>
