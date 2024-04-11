812114d507b0c24114c7ccc40d1fcb3f98297ad6
Dhemeira <peterlakics2@gmail.com>
2024-04-11 11:25:00 +0200

    Refactor custom properties

--- src/App.vue
+++ src/App.vue
@@ -28,22 +28,5 @@ const hue = ref(210);
 <style scoped>
 .wrapper {
   --hue: v-bind(hue);
-  --bkg: var(--hue) 50% 94%;
-  --text: var(--hue) 80% 20%;
-  --bkg-dark: var(--hue) 40% 10%;
-  --text-dark: var(--hue) 80% 90%;
-  background-color: hsl(var(--bkg));
-  color: hsl(var(--text));
-}
-
-@media (prefers-color-scheme: dark) {
-  .wrapper {
-    background-color: hsl(var(--bkg-dark));
-    color: hsl(var(--text-dark));
-  }
-}
-
-.router-link-exact-active {
-  @apply text-xl font-bold;
 }
 </style>
